c3b066d531b245e49a72fd7aae67f152df97ea64
sjarifhd <sjarifhd@efishery.com>
2020-12-06 06:11:51 +0700

    change init to c++

--- main/main.cpp
+++ main/main.cpp
@@ -4,13 +4,12 @@
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
    CONDITIONS OF ANY KIND, either express or implied.
 */
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
-void app_main()
+extern "C" void app_main()
 {
     printf("Hello world!\n");
 
